3eecdc164a85f83c08cb7672db1f5110792ae2b1
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-12-07 09:18:47 -0300

    aula 5 - v-for

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,25 @@
   <div v-if="accessLevel === 'admin'">Usuário Admin</div>
   <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
   <div v-else>Usuário Normal</div>
-
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <hr />
+  <!-- <div>
+    <div v-for="obj in todos" :key="obj.id">
+      {{ obj.title }}
+    </div>
+  </div> -->
+  <div>
+    <div class="todos-item" v-for="(obj, index) in todos" :key="index">
+      {{ index }} - {{ obj.title }}
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import TheHeader from "./components/TheHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     TheHeader,
   },
   data() {
@@ -31,17 +37,56 @@ export default {
       lastName: "Capelari",
       showName: false,
       accessLevel: "marketing",
+
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: "delectus aut autem",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "quis ut nam facilis et officia qui",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "fugiat veniam minus",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: "et porro tempora",
+          completed: true,
+        },
+        {
+          userId: 1,
+          id: 5,
+          title:
+            "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          completed: false,
+        },
+      ],
     };
   },
 };
 </script>
 
 <style>
+.todos-item {
+  background: #000;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: #fff;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
